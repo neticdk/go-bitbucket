0d25776dd94aaf3aec34789026880637ec83c78c
Thor Anker Kvisgård Lange <tal@netic.dk>
2025-01-06 08:32:07 +0100

    feat: Added Projects list function (#4)
    * feat: Added Projects list function
    
    * feat: Added ListProjects

--- bitbucket/projects.go
+++ bitbucket/projects.go
@@ -1,5 +1,30 @@
 package bitbucket
 
+import (
+	"context"
+)
+
 type ProjectsService service
 
 const projectsApiName = "api"
+
+type Project struct {
+	ID    uint64            `json:"id,omitempty"`
+	Key   string            `json:"key,omitempty"`
+	Name  string            `json:"name"`
+	Links map[string][]Link `json:"links,omitempty"`
+}
+
+type ProjectList struct {
+	ListResponse
+	Projects []*Project `json:"values"`
+}
+
+func (s *ProjectsService) ListProjects(ctx context.Context, opts *ListOptions) ([]*Project, *Response, error) {
+	var l ProjectList
+	resp, err := s.client.GetPaged(ctx, projectsApiName, "projects", &l, opts)
+	if err != nil {
+		return nil, resp, err
+	}
+	return l.Projects, resp, nil
+}
